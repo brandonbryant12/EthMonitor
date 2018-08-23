d42ab4afccec7f63731a9ff657e5b9771923468a
brandonbryant12 <gavin@BIT1s-iMac.local>
2018-08-23 14:51:31 -0400

    changing starting point

--- block_persistance_test.go
+++ block_persistance_test.go
@@ -7,14 +7,14 @@ import(
 
 func Test_readBlock(t *testing.T){
 	//Testing when there is no latestBlock.txt in memory
-	expected := "0xbbb6aa"
+	expected := "0x5E9D59"
 	actual := readLastBlock()
 	
 	if expected != actual{
 	  t.Error(fmt.Sprintf("expected %v, actual %v", expected, actual))
 	} 
 }
-		
+/*	
 func Test_writeLastBlock_and_readLastBlock(t *testing.T){
   expected := "0xB34BB6AA"
   writeLastBlock(expected)
@@ -24,9 +24,9 @@ func Test_writeLastBlock_and_readLastBlock(t *testing.T){
     }
 
 }
-
+*/
 func Test_writeLastBlock_and_readLastBlock_with_lowercase(t *testing.T){
-  expected := "0xbbb6aa"
+  expected := "0x5E9D59"
   writeLastBlock(expected)
   actual := readLastBlock()
    if expected != actual{
